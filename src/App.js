d0663caf2a052aab36b25ce649ea75cd24a4b438
hashilbasheer <hashilbasheer@gmail.com>
2023-03-23 15:20:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Kenessa!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          CPQ DATA NON PROD JOB TEST UPDATED.
+        </p>
       </header>
     </div>
   );
