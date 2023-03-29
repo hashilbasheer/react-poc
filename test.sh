d0f2becc396212e37366af422ef828524bd5dae4
hashilbasheer <hashilbasheer@gmail.com>
2023-03-30 01:07:26 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install Selenium and ChromeDriver
+pip install selenium
+apt-get update
+apt-get install -y chromium-browser
+wget https://chromedriver.storage.googleapis.com/93.0.4577.15/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
+
+# Run the test case
+python test.py
