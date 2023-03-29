4fc6778beaa6016f1049546133ccad189b95ebc9
hashilbasheer <hashilbasheer@gmail.com>
2023-03-30 02:11:33 +0530

    Update health_check.py

--- health_check.py
+++ health_check.py
@@ -1,8 +1,27 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
 
-driver = webdriver.Chrome('./chromedriver')
-driver.get("http://volvosalescockpit.6ed500daefb04e85a911.eastus.aksapp.io/")
-print(driver.title)
-print(driver.current_url)
-driver.close()
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")
+
+chrome_driver_path = os.path.join(os.getcwd(), "drivers", "chromedriver")
+
+driver = webdriver.Chrome(chrome_driver_path, options=chrome_options)
+
+url = "https://www.example.com"
+
+driver.get(url)
+
+try:
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "my-element")))
+    print("URL is accessible.")
+except:
+    print("URL is not accessible.")
+
+driver.quit()
