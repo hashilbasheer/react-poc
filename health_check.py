2e1853e403308a362de878d7717c5d3f11d8ac8f
hashilbasheer <hashilbasheer@gmail.com>
2023-03-30 02:06:28 +0530

    Update health_check.py

--- health_check.py
+++ health_check.py
@@ -1,11 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 
-options = Options()
-options.headless = True
-options.add_argument("--window-size=1920,1200")
-
-driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+driver = webdriver.Chrome('./chromedriver')
 driver.get("http://volvosalescockpit.6ed500daefb04e85a911.eastus.aksapp.io/")
-print(driver.page_source)
-driver.quit()
+print(driver.title)
+print(driver.current_url)
+driver.close()
