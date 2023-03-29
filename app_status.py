0178eb3a85d87ca2b64551643ffd89b880e2adc4
hashilbasheer <hashilbasheer@gmail.com>
2023-03-30 00:07:55 +0530

    Update app_status.py

--- app_status.py
+++ app_status.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import sys
 
-app_url = sys.argv[1]
+app_url = sys.argv[0]
 
 caps = DesiredCapabilities.FIREFOX.copy()
 caps["marionette"] = True
@@ -11,7 +11,7 @@ browser = webdriver.Firefox(capabilities=caps)
 browser.get(app_url)
 
 # Check that the application is up and running
-assert browser.title == "My Application"
+assert browser.title == "React App1"
 print("Application is up and running!")
 
 browser.quit()
