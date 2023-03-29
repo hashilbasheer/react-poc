28577624c58364a5713eef5540d6940a78bf5964
hashilbasheer <hashilbasheer@gmail.com>
2023-03-30 02:21:55 +0530

    Update and rename health_check.py to my_selenium_test.py

---
+++ my_selenium_test.py
@@ -0,0 +1,31 @@
+import sys
+import time
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+app_url = sys.argv[0]
+driver_path = sys.argv[1]
+
+caps = DesiredCapabilities.CHROME.copy()
+caps["goog:loggingPrefs"] = {"browser": "ALL"}
+
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-browser-side-navigation')
+
+browser = webdriver.Chrome(options=options, executable_path=driver_path, desired_capabilities=caps)
+
+def check_application_status():
+    try:
+        browser.get(app_url)
+        while browser.execute_script('return document.readyState') != 'complete':
+            time.sleep(1)
+        assert browser.title == "React App1"
+        print("Application is up and running!")
+    except Exception as e:
+        print("Error: {}".format(str(e)))
+
+check_application_status()
+browser.quit()
