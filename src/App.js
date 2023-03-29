d8a4322588a70024c4ee422dcfd92d3fbf886ee0
hashilbasheer <hashilbasheer@gmail.com>
2023-03-29 22:21:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <p>
           Welcome to VOLVO SALES COCKPIT.
+            <input type="hidden" defaultValue={process.env.GITHUB_RUN_NUMBER} />
         </p>
       </header>
     </div>
