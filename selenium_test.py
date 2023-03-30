5f4a955824f9ae767f5a2c2d60ff96a06a04c9d1
hashilbasheer <hashilbasheer@gmail.com>
2023-03-30 20:21:46 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -1,48 +1,29 @@
+import subprocess
 from selenium import webdriver
-import os
 
-try:
-    # Define the URL you want to test
-    URL = "http://volvosalescockpit.6ed500daefb04e85a911.eastus.aksapp.io//"
+# Define the URL of the webpage you want to visit
+url = "http://volvosalescockpit.6ed500daefb04e85a911.eastus.aksapp.io/"
 
-    # Define the path to the chromedriver executable
-    DRIVER_PATH = "/usr/local/bin/chromedriver"
+# Define the path to the chromedriver executable
+driver_path = "/usr/local/bin/chromedriver"
 
-    # Set the Github Actions secret token as an environment variable
-    GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+# Install the requests module using pip
+subprocess.check_call(["pip", "install", "requests"])
 
-    # Create a new Chrome webdriver
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless') # Run Chrome in headless mode
-    driver = webdriver.Chrome(executable_path=DRIVER_PATH, options=options)
+# Create a new Chrome webdriver
+driver = webdriver.Chrome(executable_path=driver_path)
 
-    # Visit the URL
-    driver.get(URL)
+# Visit the webpage
+driver.get(url)
 
-    # Find the page title element and assert that it contains the expected text
-    assert "React App1" in driver.title
+# Get the current URL after any redirects
+current_url = driver.current_url
 
-    # Print a success message
-    print("Test passed!")
+# Get the status code of the current URL
+status_code = requests.get(current_url).status_code
 
-    # Set the Github Actions status to "success"
-    os.system(f'curl -H "Authorization: token {GITHUB_TOKEN}" \
-                  --request POST \
-                  --data \'{{"state": "success"}}\' \
-                  https://api.github.com/repos/{os.environ["GITHUB_REPOSITORY"]}/statuses/{os.environ["GITHUB_SHA"]}')
+# Print the status code
+print(status_code)
 
-except AssertionError:
-    # If the assertion fails, print an error message and set the Github Actions status to "failure"
-    print("Error: page title does not contain expected text")
-    os.system(f'curl -H "Authorization: token {GITHUB_TOKEN}" \
-                  --request POST \
-                  --data \'{{"state": "failure", "description": "Page title does not contain expected text"}}\' \
-                  https://api.github.com/repos/{os.environ["GITHUB_REPOSITORY"]}/statuses/{os.environ["GITHUB_SHA"]}')
-
-except Exception as e:
-    # Print any exceptions that occur during the script run and set the Github Actions status to "failure"
-    print(f"Error: {e}")
-    os.system(f'curl -H "Authorization: token {GITHUB_TOKEN}" \
-                  --request POST \
-                  --data \'{{"state": "failure", "description": "Error occurred during test run"}}\' \
-                  https://api.github.com/repos/{os.environ["GITHUB_REPOSITORY"]}/statuses/{os.environ["GITHUB_SHA"]}')
+# Close the webdriver
+driver.quit()
