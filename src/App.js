420c00f76115a48ee8668cb142fb0edd1fd8454c
hashilbasheer <hashilbasheer@gmail.com>
2023-03-30 13:04:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Welcome to VOLVO SALES COCKPIT with GH_RUN_NUMBER: {process.env.GITHUB_RUN_NUMBER}
+          Welcome to VOLVO SALES COCKPIT !! The Build Number is : {process.env.GITHUB_RUN_NUMBER}
         </p>
       </header>
     </div>
