c2e6a9ed0053cff6bc6fb6fb8fa00ac9bf8f30cd
hashilbasheer <hashilbasheer@gmail.com>
2023-03-30 20:05:07 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 # Define the URL of the webpage you want to visit
 url = "http://volvosalescockpit.6ed500daefb04e85a911.eastus.aksapp.io/"
@@ -6,9 +7,12 @@ url = "http://volvosalescockpit.6ed500daefb04e85a911.eastus.aksapp.io/"
 # Define the path to the chromedriver executable
 driver_path = "/usr/local/bin/chromedriver"
 
+# Create a ChromeOptions object and set it to run in headless mode
+chrome_options = Options()
+chrome_options.add_argument('--headless')
 
-# Create a new Chrome webdriver
-driver = webdriver.Chrome(executable_path=driver_path)
+# Create a new Chrome webdriver with the specified options
+driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
 
 # Visit the webpage
 driver.get(url)
@@ -21,7 +25,3 @@ print(title)
 
 # Close the webdriver
 driver.quit()
-
-
-
-
